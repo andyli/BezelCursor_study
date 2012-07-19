9eccf4986b4814ad6d8351541c08cff8ac3989e2
Andy Li <andy@onthewings.net>
2012-07-20 03:58:43 +0800

    init project

---
+++ src/net/onthewings/mobzor/Main.hx
@@ -0,0 +1,38 @@
+package net.onthewings.mobzor;
+
+import com.haxepunk.Engine;
+import com.haxepunk.HXP;
+import nme.display.Sprite;
+import nme.events.KeyboardEvent;
+import nme.ui.Keyboard;
+import nme.Lib;
+
+class Main extends Engine {
+	
+	public function new():Void {
+		super(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight, 30, false);
+	}
+
+	override public function init():Void {		
+		HXP.screen.color = 0x333333;
+		HXP.screen.scale = 1;
+		
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp);
+		
+		//HXP.world = 
+		
+	}
+	
+	function onKeyUp(evt:KeyboardEvent):Void {
+		switch(evt.keyCode) {
+			case Keyboard.ESCAPE:
+				HXP.console.enable();
+		}
+		
+		trace("Key Up: " + evt.keyCode);
+	}
+	
+	public static function main():Void {
+		Lib.current.addChild(new Main());
+	}
+}
