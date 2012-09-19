257fc2d3129faeb9481dbf44028b360e9b35acf3
Andy Li <andy@onthewings.net>
2012-09-20 03:15:40 +0800

    Added missed msg.

--- src/bezelcursor/world/TestTouchWorld.hx
+++ src/bezelcursor/world/TestTouchWorld.hx
@@ -1,11 +1,13 @@
 package bezelcursor.world;
 
-import hsl.haxe.Signal;
-import com.haxepunk.HXP;
+import hsl.haxe.*;
+import com.haxepunk.*;
+import com.haxepunk.graphics.*;
 using com.eclecticdesignstudio.motion.Actuate;
 import nme.display.Sprite;
 import nme.events.TouchEvent;
 import nme.geom.*;
+import nme.text.*;
 using org.casalib.util.ArrayUtil;
 
 import bezelcursor.cursor.*;
@@ -20,6 +22,8 @@ class TestTouchWorld extends GameWorld {
 	public var targets:Array<Target>;
 	
 	public var startBtn:StartButton;
+	public var missedText:Text;
+	public var missedEntity:Entity;
 	
 	override public function new(taskBlockData:TaskBlockData):Void {
 		super();
@@ -38,6 +42,20 @@ class TestTouchWorld extends GameWorld {
 		}
 		
 		startBtn = new StartButton("Start");
+		
+		missedEntity = new Entity();
+		missedText = new Text("MISSED", {
+			color: 0xFF0000,
+			size: Math.round(DeviceData.current.screenDPI * 0.36),
+			resizable: true,
+			align: TextFormatAlign.CENTER
+		});
+		missedText.scrollX = missedText.scrollY = 0;
+		missedEntity.addGraphic(missedText);
+		missedEntity.width = missedText.textWidth;
+		missedEntity.height = missedText.textHeight;
+		missedEntity.x = (HXP.stage.stageWidth - missedEntity.width) * 0.5;
+		missedEntity.y = (HXP.stage.stageHeight - missedEntity.height) * 0.5;
 	}
 	
 	public function next():Void {
@@ -54,7 +72,6 @@ class TestTouchWorld extends GameWorld {
 		}
 		
 		currentTarget = targets[nextSpec.target];
-		currentTarget.onClickSignaler.bindVoid(next).destroyOnUse();
 		
 		camera.tween(0.5, nextSpec.camera).onComplete(function() startBtn.visible = true);
 	}
@@ -62,8 +79,10 @@ class TestTouchWorld extends GameWorld {
 	override public function begin():Void {
 		super.begin();
 		
-		if (HXP.engine.asMain().cursorManager.inputMethod.forThumbSpace != null) {
-			HXP.stage.addChild(HXP.engine.asMain().cursorManager.thumbSpaceView);
+		var cm = HXP.engine.asMain().cursorManager;
+		
+		if (cm.inputMethod.forThumbSpace != null) {
+			HXP.stage.addChild(cm.thumbSpaceView);
 		}
 		
 		for (target in targets) {
@@ -72,11 +91,32 @@ class TestTouchWorld extends GameWorld {
 		
 		add(startBtn);
 		
+		cm.onClickSignaler.bind(onCursorClick);
+		
 		next();
 	}
 	
 	override public function end():Void {
-		currentTarget.onClickSignaler.unbindVoid(next);
+		var cm = HXP.engine.asMain().cursorManager;
+		cm.onClickSignaler.unbind(onCursorClick);
 		super.end();
+	}
+	
+	function onCursorClick(target:Target):Void {
+		startBtn.visible = false;
+		
+		var cm = HXP.engine.asMain().cursorManager;
+		cm.cursorsEnabled = false;
+		
+		if (target == currentTarget){
+			next();
+		} else {
+			add(missedEntity);
+			missedText.alpha = 0;
+			missedText.tween(0.5, { alpha:1.0 }).onComplete(function(){ 
+				remove(missedEntity); 
+				next();
+			});
+		}
 	}
 }
